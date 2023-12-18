97e6bf8b491f5e50fe130dc3f1744240ecc4096a
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-12-18 21:42:37 +0700

    alanbtn, news

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "c9b10b56abd5fe0376c02b15545232172e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+        }
+      },
+    });
+  }, []);
+
   return (
     <div>
       <h1>Hello, ttv</h1>
